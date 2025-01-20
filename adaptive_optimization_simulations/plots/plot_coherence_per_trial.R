be25ab81a56c94bba3cff463064ef518c88b5024
shirelch <elchayani@mail.tau.ac.il>
2025-01-20 11:46:42 +0200

    fixed plotting foarmat

--- adaptive_optimization_simulations/plots/plot_coherence_per_trial.R
+++ adaptive_optimization_simulations/plots/plot_coherence_per_trial.R
@@ -11,15 +11,14 @@ plot_agent_results = function(results, title) {
   results = results |>
     mutate(Agent = paste0("t=", t, ", beta=", beta))
   
-  # Prepare a separate data frame for dashed lines (constant t values)
   dashed_lines = results |>
     group_by(Agent) |>
-    summarize(t = unique(t)) # Extract unique t values for each agent
+    summarize(t = unique(t))
   
   p = ggplot(results, aes(x = trial, y = coherence, color = Agent)) +
     geom_line() + # Main lines
     geom_hline(data = dashed_lines, aes(yintercept = t, color = Agent), 
-               linetype = "dashed", alpha = 0.1) + # Dashed lines
+               linetype = "dashed", alpha = 0.1) +
     labs(
       title = title,
       x = "Test Trial",
@@ -27,22 +26,20 @@ plot_agent_results = function(results, title) {
       color = "Agent"
     ) +
     theme_minimal() +
-    theme(legend.position = "none") # Remove the legend
+    theme(legend.position = "none")
 }
 
 for (file_path in file_paths) {
   load(file_path)
   
-  # Ensure the results data is loaded into `all_results`
   if (exists("all_results")) {
     dataset_name = tools::file_path_sans_ext(basename(file_path))
     plot_title = paste("Coherence as a Function of Test Trial -", dataset_name)
     
-    # Create interactive plot for the dataset
     p = plot_agent_results(all_results, plot_title)
     print(p)
-    # Clear all_results for the next file
-    rm(all_results)
+
+        rm(all_results)
   } else {
     warning(paste("Results not found in", file_path))
   }
