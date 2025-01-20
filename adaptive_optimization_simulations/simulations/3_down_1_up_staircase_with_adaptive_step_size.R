8d1e3033430de01360ddafe278c77c7f9d0ceed2
shirelch <elchayani@mail.tau.ac.il>
2025-01-20 12:59:55 +0200

    fixed bug in adaptive step size simulation
    reset step size foreach agent, bigger step size reductions

--- adaptive_optimization_simulations/simulations/3_down_1_up_staircase_with_adaptive_step_size.R
+++ adaptive_optimization_simulations/simulations/3_down_1_up_staircase_with_adaptive_step_size.R
@@ -2,7 +2,6 @@ load("./adaptive_optimization_simulations/data/agents.Rdata")
 load("./adaptive_optimization_simulations/data/config.RData")
 
 all_results = data.frame()
-step_size = 30
 
 for (i in seq_len(nrow(agents))) {
   agent = agents[i, ]
@@ -10,6 +9,7 @@ for (i in seq_len(nrow(agents))) {
   beta = agent$beta
   lambda = agent$lambda
   
+  step_size = 30
   coherence = config$initial_coherence
   df             = data.frame()
   
@@ -19,7 +19,7 @@ for (i in seq_len(nrow(agents))) {
   
   for (trial in 1:config$Ntrials) {
     if (reversals == 1) {
-      step_size = step_size - step_size / 5
+      step_size = step_size / 2
       reversals = 0
     }
     
@@ -58,7 +58,6 @@ for (i in seq_len(nrow(agents))) {
     # Append data
     df = rbind(df, data.frame(trial, acc = acc, coherence = coherence, prob_correct = prob_correct, direction = direction, 
                               t = t, beta = beta))
-  trial = trial + 1
     }
   
   # Store results for each parameter combination
@@ -66,5 +65,5 @@ for (i in seq_len(nrow(agents))) {
   }
 
 
-save(all_results, file = "./adaptive_optimization_simulations/data/simulation_resultls/3_down_1_up_adaptive_step_size_simulation_results.RData")
+save(all_results, file = "./adaptive_optimization_simulations/data/simulation_results/3_down_1_up_adaptive_step_size_simulation_results.RData")
 
