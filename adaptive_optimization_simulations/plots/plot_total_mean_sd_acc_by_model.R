7635a0509e6baee72a8885c69da6eacb289428eb
shirelch <elchayani@mail.tau.ac.il>
2025-01-20 12:41:43 +0200

    added sd plot

--- adaptive_optimization_simulations/plots/plot_total_mean_sd_acc_by_model.R
+++ adaptive_optimization_simulations/plots/plot_total_mean_sd_acc_by_model.R
@@ -7,6 +7,7 @@ file_paths = list.files(
 )
 
 total_mean_acc = data.frame(Model = character(), MeanAccuracy = numeric(), stringsAsFactors = FALSE)
+total_sd_acc = data.frame(Model = character(), MeanAccuracy = numeric(), stringsAsFactors = FALSE)
 
 for (file_path in file_paths) {
   load(file_path)
@@ -15,12 +16,18 @@ for (file_path in file_paths) {
     dataset_name = tools::file_path_sans_ext(basename(file_path))
     
     mean_acc = mean(all_results$acc, na.rm = TRUE)
+    sd_acc = sd(all_results$acc, na.rm = TRUE)
     dataset_name = sub("_simulation_results", "", dataset_name)
     
     total_mean_acc = rbind(
       total_mean_acc,
       data.frame(Model = dataset_name, MeanAccuracy = mean_acc)
     )
+    
+    total_sd_acc = rbind(
+      total_sd_acc,
+      data.frame(Model = dataset_name, SDAccuracy = sd_acc)
+    ) 
     
     rm(all_results)
   } else {
@@ -37,6 +44,22 @@ p = ggplot(total_mean_acc, aes(x = Model, y = MeanAccuracy, fill = Model)) +
     title = "Total Mean Accuracy Per Model",
     x = "Model",
     y = "Total Mean Accuracy"
+  ) +
+  theme_minimal() +
+  theme(axis.text.x = element_text(angle = 45, hjust = 1)) +
+  theme(legend.position = "none")
+
+print(p)
+
+p = ggplot(total_sd_acc, aes(x = Model, y = SDAccuracy, fill = Model)) +
+  geom_bar(stat = "identity") +
+  geom_text(aes(label = round(SDAccuracy, 3)), 
+            vjust = -0.3,
+            size = 3) +
+  labs(
+    title = "Total SD Accuracy Per Model",
+    x = "Model",
+    y = "Total SD Accuracy"
   ) +
   theme_minimal() +
   theme(axis.text.x = element_text(angle = 45, hjust = 1)) +
