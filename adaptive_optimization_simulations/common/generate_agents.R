120bfc4d01be0eeab6bfd543f360d69237776a48
shirelch <elchayani@mail.tau.ac.il>
2025-01-21 14:39:59 +0200

    100 trials

--- adaptive_optimization_simulations/common/generate_agents.R
+++ adaptive_optimization_simulations/common/generate_agents.R
@@ -1,10 +1,12 @@
+load("./adaptive_optimization_simulations/data/config.RData")
+
 # Set parameters
 thresholds = seq(5, 75, by = 5)
 slopes = seq(1.5, 5.5, by = 0.25)
 lapse_rate = 0
 
 agents = expand.grid(t = thresholds, beta = slopes, lambda = lapse_rate)
-agents_df = agents[rep(seq_len(nrow(agents)), each = 50), ]
-agents_df$trial = rep(1:50, times = nrow(agents))
+agents_df = agents[rep(seq_len(nrow(agents)), each = config$Ntrials), ]
+agents_df$trial = rep(1:config$Ntrials, times = nrow(agents))
 
 save(df = agents_df, file = "./adaptive_optimization_simulations/data/agents.Rdata")
