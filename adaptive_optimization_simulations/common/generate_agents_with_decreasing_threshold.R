15a3f75895781950cb65198ce921a08632edad7f
shirelch <elchayani@mail.tau.ac.il>
2025-03-18 14:39:33 +0200

    created different decrease rates for decreasing threshold simulations

--- adaptive_optimization_simulations/common/generate_agents_with_decreasing_threshold.R
+++ adaptive_optimization_simulations/common/generate_agents_with_decreasing_threshold.R
@@ -6,9 +6,16 @@ thresholds = runif(Nsubjects, 1, 30)
 slopes = runif(Nsubjects, 0.5,8)
 lapse_rate = 0
 
-agents = data.frame(id = seq(1,Nsubjects), t = thresholds, beta = slopes, lambda = lapse_rate)
-agents_decreasing_threshold_df = agents[rep(seq_len(nrow(agents)), each = config$Ntrials),]
-agents_decreasing_threshold_df$trial = rep(1:config$Ntrials, times = nrow(agents))
-agents_decreasing_threshold_df$t = agents_decreasing_threshold_df$t - 0.01 * (agents_decreasing_threshold_df$trial - 1)
+Ntrials = 100 # Fixed value of Ntrials, config contains a vector
 
-save(df = agents_decreasing_threshold_df, file = "./adaptive_optimization_simulations/data/agents_with_decreasing_threshold.Rdata")
+decrease_rates = c(0.01,0.05,0.1)
+
+for (d in decrease_rates) {
+  agents = data.frame(id = seq(1,Nsubjects), t = thresholds, beta = slopes, lambda = lapse_rate)
+  agents_decreasing_threshold_df = agents[rep(seq_len(nrow(agents)), each = Ntrials),]
+  agents_decreasing_threshold_df$trial = rep(1:Ntrials, times = nrow(agents))
+  agents_decreasing_threshold_df$t = agents_decreasing_threshold_df$t - 0.01 * (agents_decreasing_threshold_df$trial - 1)
+  
+  save(df = agents_decreasing_threshold_df, file = paste0("./adaptive_optimization_simulations/data/agents_with_decreasing_threshold/agents_with_decreasing_threshold_decrease_rate_", d, ".Rdata"))
+  
+}
